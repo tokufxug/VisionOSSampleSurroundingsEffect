68ea7a654e87199c5b377335e91c7cb8f7ab275a
tokufxug <tokufxug@gmail.com>
2024-07-06 14:42:37 +0900

    commit

--- VisionOSSampleSurroundingsEffect/ContentView.swift
+++ VisionOSSampleSurroundingsEffect/ContentView.swift
@@ -7,17 +7,12 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
+            Text("Color change by Surroundings Effect.")
             ToggleImmersiveSpaceButton()
         }
         .padding()
--- VisionOSSampleSurroundingsEffect/ImmersiveView.swift
+++ VisionOSSampleSurroundingsEffect/ImmersiveView.swift
@@ -7,18 +7,38 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
 struct ImmersiveView: View {
 
+    @State private var color = Color.red
+    @State private var colorIndex = 0
+    private let colors: [Color] = [
+            .red, .blue, .green, .yellow, .orange, .purple, .pink, .cyan,
+            .black, .brown, .indigo
+    ]
+
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
+            if let entity = try? await Entity(named: "pancakes") {
+                entity.position.y = 1.0
+                entity.position.z = -1.5
+                entity.scale*=5
+                entity.components.set(PointLightComponent(color: .white))
+                content.add(entity)
+            }
+        }
+        .preferredSurroundingsEffect(
+            SurroundingsEffect.colorMultiply(color))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                var nextIndex = Int.random(in: 0..<colors.count)
+                if colorIndex == nextIndex {
+                    nextIndex = nextIndex + 1 >= colors.count ? 0 : nextIndex + 1
+                }
+                colorIndex = nextIndex
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    color = colors[nextIndex]
+                }
             }
         }
     }
